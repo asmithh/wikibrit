1c60dd95ce6f71bb114b14470e390811b345408c
Alyssa Smith <wintermute@eliza.ddns.net>
2021-05-18 11:54:53 -0400

    commenting for clarity

--- get_data.py
+++ get_data.py
@@ -7,9 +7,11 @@ import pywikibot
 
 # conn = elasticsearch.Urllib3HttpConnection(host='https://eliza.ddns.net', port=9200, ssl_version='SSLv2')
 # es = Elasticsearch(connection=conn)
+# change elasticsearch host if we're dumping to ES again; otherwise get rid of references to ES.
 es = Elasticsearch(hosts=['http://eliza.ddns.net:9200'])
 INDEX = 'britney-test'
 
+# get text/comment/size/timestamp/username of revision. 
 class WikiRevision(object):
 	def __init__(self, text, comment, size, timestamp, user, previous_id=0, future_id=0):
 		self.text = text
@@ -18,9 +20,11 @@ class WikiRevision(object):
 		self.timestamp = timestamp
 		self.user = user
 
+# revisions are set up as kind of a linkedlist inside ES, so this is the "initial" entry.
 old_rev = WikiRevision('', '', 0, '1970-01-01T00:00:00Z', '', previous_id=None, future_id=1)
 es.index(index=INDEX, id=0, body=old_rev.__dict__)
 
+# setting up pywikibot. there aren't entries before ~2006 iirc.
 site = pywikibot.Site("en", "wikipedia")
 page = pywikibot.Page(site, "Britney Spears")
 starttime = pywikibot.Timestamp(year=2021, month=6, day=1)
@@ -34,14 +38,18 @@ revs_all = []
 for idx, rev in enumerate(revs):
 	try:
 		current_rev = WikiRevision(rev.text, rev.comment, rev.size, rev.timestamp, rev.user, previous_id=idx, future_id=idx + 2)
-# 		with open(f'./revisions/{rev.timestamp}_britney_spears.txt', 'w') as f:
-# 			f.write(current_rev.comment + '\n')
-# 			f.write(current_rev.user + '\n')
-# 			f.write(current_rev.text)
-# 		es.index(index=INDEX, id=idx + 1, body=current_rev.__dict__)
+		# use this block if you want to write the full version files to .txt files. this will use a lot of space!!
+		with open(f'./revisions/{rev.timestamp}_britney_spears.txt', 'w') as f:
+			f.write(current_rev.comment + '\n')
+			f.write(current_rev.user + '\n')
+			f.write(current_rev.text)
+		# indexing to elasticsearch - this makes our metadata & edits easily query-able & searchable! 
+		# (though it isn't strictly necessary)
+		es.index(index=INDEX, id=idx + 1, body=current_rev.__dict__)
 		revs_all.append(current_rev.__dict__)
 	except:
+		# this should be rare!!!
 		print(rev.timestamp)
 
-
+# dumping all our metadata (about 1.8GB uncompressed)
 pickle.dump(revs_all, open('./pickles/rev_metadata.pkl', 'wb'))
--- process_data.py
+++ process_data.py
@@ -12,11 +12,16 @@ from nltk import word_tokenize
 from nltk.corpus import stopwords
 from nltk.stem.wordnet import WordNetLemmatizer
 
+# crappy attempt at getting LDA to work on these documents. takes a long time to run.
 
+# best_n_clusters is a total guess. feel free to change. 
 best_n_clusters = 10
+
+# these tend to work pretty well as presets
 stopwords = stopwords.words("english")
 lemmatizer = WordNetLemmatizer()
 
+# feel free to mess with the punctuation we remove/don't remove
 punctuation_no_underscore = set(string.punctuation)
 punctuation_no_underscore.add("’")
 punctuation_no_underscore.add("”")
@@ -26,35 +31,44 @@ my_dict = corpora.Dictionary()
 pickles = []
 filter_keys = {"="}
 
+# ./revisions is the set of all revisions as .txt files
 L = len(os.listdir('./revisions'))
 tot = 0
 for wiki_version in os.listdir('./revisions'):
 	if tot % 500 == 0:
 		print(tot / L)
+		# just to keep track of our progress. there are a bunch of revisions!!
 	tot += 1
 	words = []
 	with open(os.path.join('./revisions', wiki_version)) as f:
 		for line in f.readlines():
 			line = re.sub("[^a-zA-Z ]+", "", line)
+			# we're getting rid of anything that isn't a letter - this is a bit severe and we can mess with it
 			words.extend(line.split())
 	content_no_punctuation = [word.lower() for word in words]
     # here we remove stopwords
 	content_no_stopwords = [word for word in content_no_punctuation if word not in stopwords]
+	# lemmatizing (removing suffixes/verb forms to get at the root forms of words iirc)
 	content_lemmatized = [lemmatizer.lemmatize(word) for word in content_no_stopwords]
+	# getting rid of small words
 	content_not_short = [word for word in content_lemmatized if len(word) > 2]
+	# pickling bag-of-words files for easier access later/keyword search
 	pickle_filename = 'article_pickles/' + wiki_version[:-4] + '.pkl'
 	pickle.dump(content_not_short, open(pickle_filename, 'wb'))
 	pickles.append(pickle_filename)
+	# adding bag-of-words document to dictionary
 	my_dict.add_documents([content_not_short])
 
 my_dict.filter_extremes(no_below=int(len(os.listdir('./revisions')) / 50), no_above=0.9)
-# my_dict.filter_extremes(no_below=2, no_above=0.5)
+# get rid of common/rare words
 
 corpus = []
 for pickle_file in pickles:
 	text = pickle.load(open(pickle_file, 'rb'))
+	# bag-of-words-ifying documents
 	corpus.append(my_dict.doc2bow(text))
 
+# creating & training LDA (latent dirichlet allocation) model for document clustering
 lda_model = models.LdaModel(
     corpus=corpus,
     num_topics=best_n_clusters,
@@ -63,6 +77,7 @@ lda_model = models.LdaModel(
 
 topic_keywords = []	
 pickle.dump(lda_model, open('lda_model.pkl', 'wb'))
+# try to distinguish topics by keywords (pretty crappy right now)
 for idx, topic in lda_model.show_topics(
     num_topics=10, num_words=20, formatted=False
 ):
@@ -72,6 +87,3 @@ for idx, topic in lda_model.show_topics(
     topic_keywords.append([w[0] for w in topic])
 
 pickle.dump(topic_keywords, open('topic_keywords.pkl', 'wb'))
-
-topics_by_doc = lda_model.load_document_topics()
-print(topics_by_doc)
